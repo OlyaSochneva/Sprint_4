5e825e48c3b03973efd83d191663d8ba55b36b63
OlyaSochneva <136125155+OlyaSochneva@users.noreply.github.com>
2024-01-26 17:38:16 +0400

    Tests for BooksCollector class

--- tests.py
+++ tests.py
@@ -1,24 +1,58 @@
 from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_book_added_without_genre_has_empty_value(self, setup):
+        assert setup.get_book_genre('Дюна') == ''
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    incorrect_book_names = ['', 'Книга с очень очень очень длинным названием']
+    @pytest.mark.parametrize('book_name', incorrect_book_names)
+    def test_add_new_book_incorrect_named_book_is_not_added(self, setup, book_name):
+        setup.add_new_book(book_name)
+        assert len(setup.get_books_genre()) == 1
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_books_genre_with_one_added_book_returns_correct(self, setup):
+        assert setup.get_books_genre() == {'Дюна': ''}
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre_genre_out_of_list_is_not_set(self, setup):
+        setup.set_book_genre('Дюна', 'Научная фантастика')
+        assert setup.get_book_genre('Дюна') == ''
+
+    def test_get_book_genre_genre_got_by_name_is_correct(self, setup):
+        setup.set_book_genre('Дюна', 'Фантастика')
+        assert setup.get_book_genre('Дюна') == 'Фантастика'
+
+
+    genre_list = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+    @pytest.mark.parametrize('genre', genre_list)
+    def test_get_books_with_specific_genre_returned_list_for_each_genre_is_correct(self, setup, genre):
+        setup.add_new_book('тестовая книга')
+        setup.set_book_genre('тестовая книга', genre)
+        assert len(setup.get_books_with_specific_genre(genre)) == 1
+
+    def test_get_books_for_children_books_without_genre_not_append(self, setup):
+        assert len(setup.get_books_for_children()) == 0
+
+
+    name_genre_list = [['Сияние', 'Ужасы'], ['Немезида', 'Детективы']]
+    @pytest.mark.parametrize('name, genre', name_genre_list)
+    def test_get_books_for_children_books_with_age_rating_not_append(self, setup, name, genre):
+        setup.add_new_book(name)
+        setup.set_book_genre(name, genre)
+        assert len(setup.get_books_for_children()) == 0
+
+    def test_add_book_in_favorites_same_book_not_added_twice(self, setup):
+        setup.add_book_in_favorites('Дюна')
+        setup.add_book_in_favorites('Дюна')
+        assert len(setup.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites_book_removed_successfully(self, setup):
+        setup.add_book_in_favorites('Дюна')
+        setup.delete_book_from_favorites('Дюна')
+        assert 'Дюна' not in setup.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_with_one_added_book_returns_correct(self, setup):
+        setup.add_book_in_favorites('Дюна')
+        assert setup.get_list_of_favorites_books() == ['Дюна']
+
